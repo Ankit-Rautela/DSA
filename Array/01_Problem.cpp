d36d46f2c5fdc73dc55da5187a905ecc39950f49
Ankit Rautela <ankitrautela044@gmail.com>
2024-05-12 06:41:34 +0000

    Approach 2 added

--- Array/01_Problem.cpp
+++ Array/01_Problem.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/*
+// Approach 1: Traverse reverse array and store in another new array
 class Solution {
 public:
     string reverseWord(string str) {
@@ -11,6 +13,25 @@ public:
         return result;
     }
 };
+*/
+
+// Approach 2: Use 2 pointer concept and swapping 
+class Solution
+{
+    public:
+    string reverseWord(string str) {
+        int n = str.length();
+        int start = 0;
+        int end = n - 1;
+        while(start <= end) {
+            swap(str[start],str[end]);
+            start++;
+            end--;
+        }
+        return str;
+    }
+};
+
 
 int main() {
     int t;
