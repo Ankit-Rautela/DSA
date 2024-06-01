eea667a5461cd52927c148f2e28894d9a9bf6b86
Ankit Rautela <ankitrautela044@gmail.com>
2024-06-01 02:51:03 +0000

    10

--- Array/10_Problem.cpp
+++ Array/10_Problem.cpp
@@ -1,24 +1,39 @@
-class Solution {
-public:
-    vector<int> generateRow(int row){
-        long long ans = 1;
-        vector<int> ansRow;
+#include <bits/stdc++.h>
+using namespace std;
 
-        ansRow.push_back(1);
-        for(int col=1; col<row; col++){ 
-            ans = ans * (row - col); 
-            ans = ans / (col); 
-            ansRow.push_back(ans); 
-        }
-        return ansRow;
-    }
-    vector<vector<int>> generate(int numRows) {
+vector<int> generateRow(int row) {
+    long long ans = 1;
+    vector<int> ansRow;
+    ansRow.push_back(1); //inserting the 1st element
 
-        vector<vector<int>> ans;
-        for(int i=1; i<=numRows; i++){
-            vector<int> temp = generateRow(i);
-            ans.push_back(temp);
-        }
-        return ans;
+    //calculate the rest of the elements:
+    for (int col = 1; col < row; col++) {
+        ans = ans * (row - col);
+        ans = ans / col;
+        ansRow.push_back(ans);
     }
-};
+    return ansRow;
+}
+
+vector<vector<int>> pascalTriangle(int n) {
+    vector<vector<int>> ans;
+
+    //store the entire pascal's triangle:
+    for (int row = 1; row <= n; row++) {
+        ans.push_back(generateRow(row));
+    }
+    return ans;
+}
+
+int main()
+{
+    int n = 5;
+    vector<vector<int>> ans = pascalTriangle(n);
+    for (auto it : ans) {
+        for (auto ele : it) {
+            cout << ele << " ";
+        }
+        cout << "n";
+    }
+    return 0;
+}
